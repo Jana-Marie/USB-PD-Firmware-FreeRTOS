ba2da65f4ed5c89d4f81b429368f8e2cae8dd733
Jan--Henrik <janhenrik@janhenrik.org>
2019-02-14 23:57:06 +0100

    added pulldown

--- USB-PD-Otterfirmware/Src/init.c
+++ USB-PD-Otterfirmware/Src/init.c
@@ -232,7 +232,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : BUTTON_Pin */
   GPIO_InitStruct.Pin = BUTTON_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(BUTTON_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : INT_N_Pin */
